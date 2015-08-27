0b50379a0a7a5901b7792285be9c11447abf7f0c
andy@mobilefringe.com <andy@mobilefringe.com>
2015-08-27 13:37:50 -0400

    via kodekloud.io (55ddf3f86e6f640775050000)

--- assets/application.js
+++ assets/application.js
@@ -2,8 +2,28 @@
 
 
 
-   function test_me(){
-       alert("TEST")
-   }
+    function renderPageData(container, template, collection, type){
+            
+            var item_list = [];
+            var item_rendered = [];
+            var template_html = $(template).html();
+            Mustache.parse(template_html);   // optional, speeds up future uses
+            
+            if (type == 'property'){
+                item_list.push(collection);    
+            }
+                
+            
+            
+            $.each( collection , function( key, val ) {
+                var rendered = Mustache.render(template_html,val);
+                item_rendered.push(rendered);
+            });
+            
+            $(container).show();
+            $(container).html(item_rendered.join(''));
+            
+            
+        }
    
     
