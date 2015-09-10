40cacf3a64c29f15d273f719fff9649abcfb9777
andy@mobilefringe.com <andy@mobilefringe.com>
2015-09-10 11:52:37 -0400

    via kodekloud.io (55ddf3f86e6f640775050000)

--- assets/application.js
+++ assets/application.js
@@ -11,7 +11,7 @@
         
         map.setMarks([{ xy: [coords["x"] - 35 + x_offset, coords["y"] - 110 + y_offset],
                   attrs: {
-                            src:  'http://assets.kodekloud.io/sites/55ddf3f86e6f640775000000/74651acfd9dc025696c5056ab3a75698/pin.png'     // image for marker
+                            src:  'http://assets.kodekloud.io/sites/55ddf3f86e6f640775000000/d8c14ddd5ad7b16b08a457f92431c96c/pin.png'     // image for marker
                           }
                 }
                 ])
